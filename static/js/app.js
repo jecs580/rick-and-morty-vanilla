a62cd089f6ab9b4f9c2b66be2b97a9770d03a773
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-08-10 22:46:48 -0400

    Creando una clase character con la informacion de los personajes

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,37 @@
-console.log("welcome");
+import API from './api.js'
+const api= new API();
+const $characterContainer= document.querySelector('#character-container')
+// const charatecterTemplete=`
+// <article class="character">
+// <div class="character-grid">
+//     <h2>Rick Sanchez</h2>
+//     <img src="" alt="">
+// </div>
+// </article>`;
+class Character{
+    constructor({name, image}){
+        this.name= name;
+        this.image=image;
+        this.render()
+    }
+    build(){
+        return `
+        <article class="character">
+        <div class="character-grid">
+            <h2>${this.name}</h2>
+            <img src="${this.image}" alt="">
+        </div>
+        </article>
+        `
+    }
+    render(){
+        $characterContainer.innerHTML = this.build();
+    }
+}
+async function initApp(initCharacterID){
+    const characterData=await api.getCharacter(initCharacterID);
+    // console.log(characterData);
+    new Character(characterData);
+}
+initApp(1);
+// api.getCharacter(1);
