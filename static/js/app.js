8df61aca373e9cad848e934409e7caf5489f9c22
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-08-13 20:57:26 -0400

    Colocando los datos desde la API y creando funcionalidad al boton de siguiente personaje

--- static/js/app.js
+++ static/js/app.js
@@ -1,37 +1,18 @@
 import API from './api.js'
+import Character from './character.js'
 const api= new API();
-const $characterContainer= document.querySelector('#character-container')
-// const charatecterTemplete=`
-// <article class="character">
-// <div class="character-grid">
-//     <h2>Rick Sanchez</h2>
-//     <img src="" alt="">
-// </div>
-// </article>`;
-class Character{
-    constructor({name, image}){
-        this.name= name;
-        this.image=image;
-        this.render()
-    }
-    build(){
-        return `
-        <article class="character">
-        <div class="character-grid">
-            <h2>${this.name}</h2>
-            <img src="${this.image}" alt="">
-        </div>
-        </article>
-        `
-    }
-    render(){
-        $characterContainer.innerHTML = this.build();
-    }
-}
+let firstCharacter=1;
+
+const $next=document.querySelector('#next');
+$next.addEventListener('click', async()=>{
+    firstCharacter=firstCharacter+1;
+    const characterData=await api.getCharacter(firstCharacter);
+    new Character(characterData);
+})
 async function initApp(initCharacterID){
     const characterData=await api.getCharacter(initCharacterID);
-    console.log(characterData);
-    // new Character(characterData);
+    // console.log(characterData);
+    new Character(characterData);
 }
-initApp(1);
+initApp(firstCharacter);
 // api.getCharacter(1);
